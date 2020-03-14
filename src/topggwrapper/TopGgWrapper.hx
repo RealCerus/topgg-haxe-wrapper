07fdf5b4273d02fbd8f5af3a6bd678cc0c4c3aa6
Cerus <zocks1703@web.de>
2020-03-14 15:05:32 +0100

    Fix doc issues in TopGgWrapper.hx

--- src/topggwrapper/TopGgWrapper.hx
+++ src/topggwrapper/TopGgWrapper.hx
@@ -1,27 +1,56 @@
 package topggwrapper;
 
-import topggwrapper.impl.TopGgWrapperImpl;
 import topggwrapper.Structs.StatsBasic;
 import topggwrapper.Structs.User;
 
+/**
+* Interface of the top.gg wrapper.
+**/
 interface TopGgWrapper {
 
-    public function setStatsBasic(serverCount:Int):Void;
+    /**
+    * Sets the amount of servers the bot is on
+    **/
+    function setStatsBasic(serverCount:Int):Void;
 
-    public function setStatsShardSingle(shardId:Int, shardTotal:Int, serverCount:Int):Void;
+    /**
+    * Sets the amount of servers of the shard
+    **/
+    function setStatsShardSingle(shardId:Int, shardTotal:Int, serverCount:Int):Void;
 
-    public function setStatsShardMulti(shardServerCounts:Array<Int>):Void;
+    /**
+    * Sets the amount of servers of the shards
+    **/
+    function setStatsShardMulti(shardServerCounts:Array<Int>):Void;
 
-    public function getStats(botId:String):StatsBasic;
+    /**
+    * Returns basic stats of the provided bot
+    **/
+    function getStats(botId:String):StatsBasic;
 
-    public function getVoters(botId:String):Array<User>;
+    /**
+    * Returns an array of users who have voted for the bot
+    **/
+    function getVoters(botId:String):Array<User>;
 
-    public function getUser(userId:String):User;
+    /**
+    * Returns a top.gg user with the given id
+    **/
+    function getUser(userId:String):User;
 
-    public function hasVoted(userId:String):Bool;
+    /**
+    * Returns whether a user with the given id has voted or not
+    **/
+    function hasVoted(userId:String):Bool;
 
-    public function isMultiplier():Bool;
+    /**
+    * Returns if there's a vote multiplier
+    **/
+    function isMultiplier():Bool;
 
-    public function getBotId():String;
+    /**
+    * Getter for the botId field
+    **/
+    function getBotId():String;
 
 }
